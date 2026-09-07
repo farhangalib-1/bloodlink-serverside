ec1b7ae7416430722ee97b8a04c004793e9eebd1
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-09-08 01:23:46 +0600

    user api is created

--- index.js
+++ index.js
@@ -35,7 +35,7 @@ run().catch(console.dir);
 app.get("/", (req, res) =>{
     res.send("Sever is running");
 })
-app.listen(process.env.PORT || 5000, () =>{
-    console.log(`server is running on port ${process.env.PORT || 5000}`);
+// app.listen(process.env.PORT || 5000, () =>{
+//     console.log(`server is running on port ${process.env.PORT || 5000}`);
 
-})
+// })
