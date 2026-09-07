90c152700f28bd5f6b31cad2d7da4662206c9cf8
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-09-08 00:50:07 +0600

    user api is updated

--- index.js
+++ index.js
@@ -25,11 +25,8 @@ async function run() {
         res.send(users);
     })
 
-
-
-
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // await client.db("admin").command({ ping: 1 });
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     
   }
