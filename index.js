ec4bbaa8ba9327d3bc140b173f9b5d4273217b0e
farhangalib-1 <farhan.galib_cumilla1@yahoo.com>
2026-09-24 01:25:11 +0600

    delete api is created

--- index.js
+++ index.js
@@ -71,6 +71,15 @@ async function run() {
     res.send(payments);
   })
 
+  app.delete("/users/:id", async (req, res) =>{
+    const id = req.params.id;
+    const query ={
+      _id: new ObjectId(id)
+    }
+    const result = await usersCollection.deleteOne(query);
+    res.send(result);
+  })
+
 
 
     // await client.db("admin").command({ ping: 1 });
